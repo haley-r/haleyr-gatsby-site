5ff51c19d5f6fd0229f44bc4c3e4a7b3024d69f2
haley-r <haleyryan77@gmail.com>
2020-04-26 14:59:57 -0500

    can change style with button

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,39 @@ import React, {Component} from "react"
 import './index.css';
 
 class Index extends Component {
+    state = {
+        currentStyleIndex: 0,
+        listOfStyles:['none', 'pinky', 'minty'],
+    }
+    changeStyle=()=>{
+        if (this.state.currentStyleIndex<this.state.listOfStyles.length-1){
+            this.setState({
+                currentStyleIndex: this.state.currentStyleIndex+1
+            })
+        }
+        else{
+            this.setState({
+                currentStyleIndex: 0
+            })
+        }
+
+    }
     render(){
         return(
-            <div>
-                <h1>Haley Ryan</h1>
-                <p>I am a full-stack software engineer in Minneapolis with a background in design and contemporary art.</p>
-                <p>Becoming a developer is part of my lifelong practice of self-motivated exploration and learning how to make things that are useful, thought-provoking, joy-inducing, and easy to share.</p>
-                <p>When I'm not coding, I'm probably taking care of my garden, filling up sketchbooks, fixing a bike I rescued from the dumpster, making collages, or trying to decide what I want to learn next.</p>
-                <p>I'm interested in the ways technology can give more than it takes, and how its reach can be more inclusive and less intrusive.</p>
-                <p>github: <a href="https://github.com/haley-r">haley-r</a></p>
-                <p>email: haleyryan0000@gmail.com</p>
+            <div className={this.state.listOfStyles[this.state.currentStyleIndex]} id="background-div">
+                <div className="button-div">
+                    <button onClick={this.changeStyle}>change style</button>
+                </div>
+
+                <div id="content-div">
+                    <h1>Haley Ryan</h1>
+                    <p>I am a full-stack software engineer in Minneapolis with a background in design and contemporary art.</p>
+                    <p>Becoming a developer is part of my lifelong practice of self-motivated exploration and learning how to make things that are useful, thought-provoking, joy-inducing, and easy to share.</p>
+                    <p>When I'm not coding, I'm probably taking care of my garden, filling up sketchbooks, fixing a bike I rescued from the dumpster, making collages, or trying to decide what I want to learn next.</p>
+                    <p>I'm interested in the ways technology can give more than it takes, and how its reach can be more inclusive and less intrusive.</p>
+                    <p>github: <a href="https://github.com/haley-r">haley-r</a></p>
+                    <p>email: haleyryan0000@gmail.com</p>
+                </div>
             </div>
         )
     }
