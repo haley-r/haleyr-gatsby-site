ebefe401a8ee95293242705079d32d6207df10cf
haley-r <haleyryan77@gmail.com>
2020-04-26 14:13:10 -0500

    turned index into class component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
-import React from "react"
+import React, {Component} from "react"
 import './index.css';
 
-export default () => (
-    <div>
-        <h1>Haley Ryan</h1>
-        <p>I am a full-stack software engineer in Minneapolis with a background in design and contemporary art.</p>
-        <p>Becoming a developer is part of my lifelong practice of self-motivated exploration and learning how to make things that are useful, thought-provoking, joy-inducing, and easy to share.</p>
-        <p>When I'm not coding, I'm probably taking care of my garden, filling up sketchbooks, fixing a bike I rescued from the dumpster, making collages, or trying to decide what I want to learn next.</p>
-        <p>I'm interested in the ways technology can give more than it takes, and how its reach can be more inclusive and less intrusive.</p>
-        <p>github: <a href="https://github.com/haley-r">haley-r</a></p>
-        <p>email: haleyryan0000@gmail.com</p>
-    </div>
-)
+class Index extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Haley Ryan</h1>
+                <p>I am a full-stack software engineer in Minneapolis with a background in design and contemporary art.</p>
+                <p>Becoming a developer is part of my lifelong practice of self-motivated exploration and learning how to make things that are useful, thought-provoking, joy-inducing, and easy to share.</p>
+                <p>When I'm not coding, I'm probably taking care of my garden, filling up sketchbooks, fixing a bike I rescued from the dumpster, making collages, or trying to decide what I want to learn next.</p>
+                <p>I'm interested in the ways technology can give more than it takes, and how its reach can be more inclusive and less intrusive.</p>
+                <p>github: <a href="https://github.com/haley-r">haley-r</a></p>
+                <p>email: haleyryan0000@gmail.com</p>
+            </div>
+        )
+    }
+}
+
+export default (Index);
