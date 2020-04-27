9230bbf15312e2ea62bbe655ac7f431bdc7cf206
haley-r <haleyryan77@gmail.com>
2020-04-26 19:51:59 -0500

    changed mod style

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import './index.css';
 class Index extends Component {
     state = {
         currentStyleIndex: 0,
-        listOfStyles:['none', 'mod', 'minty'],
+        listOfStyles:['none', 'mod', 'sporty'],
     }
     changeStyle=()=>{
         if (this.state.currentStyleIndex<this.state.listOfStyles.length-1){
@@ -30,9 +30,9 @@ class Index extends Component {
                 <div id="content-div">
                     <h1><span id="firstname">Haley</span> <span id="lastname">Ryan</span></h1>
                     <div id="main-text">
-                        <p>I'm a full-stack software engineer in Minneapolis with a background in design and contemporary art.</p>
-                        <p>Becoming a developer is part of my lifelong practice of self-motivated exploration and learning how to make things that are useful, thought-provoking, joy-inducing, and easy to share.</p>
-                        <p>When I'm not coding, I'm probably taking care of my garden, filling up sketchbooks, fixing a bike I rescued from the dumpster, making collages, or trying to decide what I want to learn next.</p>
+                        <p>I'm a full-stack software engineer in Minneapolis with a background in design and art-making.</p>
+                        <p>Becoming a developer is part of a lifelong practice of self-motivated exploration and learning how to make things that are useful, thought-provoking, joy-inducing, and easy to share.</p>
+                        <p>When I'm not coding, I'm probably taking care of my garden, filling up sketchbooks, fixing a bike I rescued from a dumpster, making collages, or trying to decide what I want to learn next.</p>
                         <p>I'm interested in the ways technology can give more than it takes, and how its reach can be more inclusive and less intrusive.</p>
                         <p>github: <a href="https://github.com/haley-r">haley-r</a></p>
                         <p id="last-paragraph">email: haleyryan0000@gmail.com</p>
