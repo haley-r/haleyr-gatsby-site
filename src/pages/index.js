2fe18c5e09864bb4b392aef4617f3bfeffa551f7
haley-r <haleyryan77@gmail.com>
2020-05-01 00:30:43 -0500

    more trying to fix mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import './index.css';
 class Index extends Component {
     state = {
         currentStyleIndex: 0,
-        listOfStyles:['none', 'mod', 'sporty', 'metal', 'rose-code'],
+        listOfStyles:['none', 'mod', 'sporty', 'metal', 'rose-code', 'modern'],
     }
     changeStyle=()=>{
         if (this.state.currentStyleIndex<this.state.listOfStyles.length-1){
