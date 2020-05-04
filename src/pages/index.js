266d8eaa135004b0140fa53819e21cc2f1e1dac0
haley-r <haleyryan77@gmail.com>
2020-05-04 00:38:22 -0500

    screen

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import './index.css';
 class Index extends Component {
     state = {
         currentStyleIndex: 0,
-        listOfStyles:['none', 'mod', 'sporty', 'metal', 'rose-code', 'modern', 'pop'],
+        listOfStyles:['none', 'mod', 'sporty', 'metal', 'rose-code', 'modern', 'pop', 'screen'],
     }
     changeStyle=()=>{
         if (this.state.currentStyleIndex<this.state.listOfStyles.length-1){
@@ -34,7 +34,7 @@ class Index extends Component {
                         <p>Becoming a developer is part of a lifelong practice of self-motivated exploration and learning how to make things that are useful, thought-provoking, joy-inducing, and easy to share.</p>
                         <p>When I'm not coding, I'm probably taking care of my garden, filling up sketchbooks, fixing a bike I rescued from a dumpster, making collages, or trying to decide what I want to learn next.</p>
                         <p>I'm interested in the ways technology can give more than it takes, and how its reach can be more inclusive and less intrusive.</p>
-                        <p>github: <a href="https://github.com/haley-r">haley-r</a></p>
+                        <p id="github">github: <a href="https://github.com/haley-r">haley-r</a></p>
                         <p id="last-paragraph">email: haleyryan0000@gmail.com</p>
                     </div>
                 </div>
