cf12ae250e635caea869aa02d62ca4f478f3e66f
haley-r <haleyryan77@gmail.com>
2020-07-15 13:16:48 -0500

    copy update, css fix (maybe) for second style on mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ class Index extends Component {
                     <div id="main-text">
                         <p>I'm a full-stack software engineer in Minneapolis with a background in design and art-making.</p>
                         <p>Becoming a developer is part of a lifelong practice of self-motivated exploration and learning how to make things that are useful, thought-provoking, joy-inducing, and easy to share.</p>
-                        <p>When I'm not coding, I'm probably taking care of my garden, filling up sketchbooks, fixing my dumpster-rescued bike (again), poring over vintage cookbooks, or trying to decide what I want to learn next.</p>
+                        <p>When I'm not coding, I'm probably taking care of my vegetable garden, fixing one of my dumpster-rescued bikes (again), poring over vintage cookbooks, or making a list of what I want to learn next.</p>
                         <p>I'm interested in the ways technology can give more than it takes, and how its reach can be more inclusive and less intrusive.</p>
                         <p id="github">github: <a href="https://github.com/haley-r">haley-r</a></p>
                         <p id="last-paragraph">email: haleyryan0000@gmail.com</p>
